528e7df19d24dbf47f5ee4801cf4905ed12e1c17
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-22 02:12:49 +0600

    Create 6Strings.py

---
+++ 6Strings.py
@@ -0,0 +1,40 @@
+#Exercise 1 Azamat
+#Use the len function to print the length of the string. Azamat
+x = "Hello World"
+print(len(x))
+
+#Exercise 2 Azamat
+#Get the first character of the string txt. Azamat
+txt = "Hello World"
+x = txt[0]
+
+#Exercise 3 Azamat
+#Get the characters from index 2 to index 4 (llo). Azamat
+txt = "Hello World"
+x = txt[2:5]
+
+#Exercise 4 Azamat
+#Return the string without any whitespace at the beginning or the end. Azamat
+txt = " Hello World "
+x = txt.strip()
+
+#Exercise 5 Azamat
+#Convert the value of txt to upper case. Azamat
+txt = "Hello World"
+txt = txt.upper()
+
+#Exercise 6 Azamat
+#Convert the value of txt to lower case. Azamat
+txt = "Hello World"
+txt = txt.lower()
+
+#Exercise 7 Azamat
+#Replace the character H with a J. Azamat
+txt = "Hello World"
+txt = txt.replace("H", "J")
+
+#Exercise 8 Azamat
+#Insert the correct syntax to add a placeholder for the age parameter. Azamat
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
