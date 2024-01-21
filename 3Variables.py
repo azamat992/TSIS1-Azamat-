b0a3972ed82741d9f5f8a28b086925108b8c56fa
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-22 02:10:50 +0600

    Create 3Variables.py

---
+++ 3Variables.py
@@ -0,0 +1,35 @@
+#Exercise 1 Azamat
+#Create a variable named carname and assign the value Volvo to it. Azamat
+carname = "Volvo"
+
+#Exercise 2 Azamat
+#Create a variable named x and assign the value 50 to it. Azamat
+x=50
+
+#Exercise 3 Azamat
+#Display the sum of 5 + 10, using two variables: x and y. Azamat
+x = 5
+y = 10
+print(x+y)
+
+#Exercise 4 Azamat
+#Create a variable called z, assign x + y to it, and display the result. Azamat
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#Exercise 5 Azamat
+#Insert the correct syntax to assign values to multiple variables in one line: Azamat
+x , y , z = "Orange", "Banana", "Cherry"
+
+#Exercise 6 Azamat
+#Insert the correct syntax to assign the same value to all three variables in one code line. Azamat
+x = y = z = "Orange"
+
+#Exercise 7 Azamat
+#Insert the correct keyword to make the variable x belong to the global scope. Azamat
+def myfunc():
+  global x
+  x = "fantastic"
+
