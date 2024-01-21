00df8a119c50589e083f10b784cb0318235d594d
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-22 02:11:26 +0600

    Create 4DataTypes.py

---
+++ 4DataTypes.py
@@ -0,0 +1,41 @@
+#Exercise 1 Azamat
+#The following code example would print the data type of x, what data type would that be? Azamat
+x = 5
+print(type(x))
+#Result: int 
+
+#Exercise 2 Azamat
+#The following code example would print the data type of x, what data type would that be?
+x = "Hello World"
+print(type(x))
+#Result: str
+
+#Exercise 3 Azamat
+#The following code example would print the data type of x, what data type would that be?
+x = 20.5
+print(type(x))
+#Result: float
+
+#Exercise 4 Azamat
+#The following code example would print the data type of x, what data type would that be?
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Result: list
+
+#Exercise 5 Azamat
+#The following code example would print the data type of x, what data type would that be?
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Result: tuple
+
+#Exercise 6 Azamat
+#The following code example would print the data type of x, what data type would that be?
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Result: dict
+
+#Exercise 7 Azamat
+#The following code example would print the data type of x, what data type would that be?
+x = True
+print(type(x))
+#Result: bool
