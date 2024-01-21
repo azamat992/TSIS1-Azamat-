81eaea6d1732b98675fda54b6c99e886f9e09219
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-22 02:08:59 +0600

    Create 1syntax.py

---
+++ 1syntax.py
@@ -0,0 +1,8 @@
+#Exercise №1 Azamat
+#Insert the missing part of the code below to output "Hello World". Azamat
+ print ("Hello World")
+
+#Exercise №2 Azamat
+#Complete the code block, print "YES" if 5 is larger than 2. Azamat
+if 5 > 2:
+    print("YES")
