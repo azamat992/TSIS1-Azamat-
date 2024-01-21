919217f873f48353310edf9219eb952adf27c227
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-01-22 02:12:16 +0600

    Create 5Numbers.py

---
+++ 5Numbers.py
@@ -0,0 +1,14 @@
+#Exercise 1 Azamat
+#Insert the correct syntax to convert x into a floating point number. Azamat
+x = 5
+x = float(x)
+
+#Exercise 2 Azamat
+#Insert the correct syntax to convert x into a integer. Azamat
+x = 5.5
+x = int(x)
+
+#Exercise 3 Azamat
+#Insert the correct syntax to convert x into a complex number. Azamat
+x = 5
+x = complex(x)
